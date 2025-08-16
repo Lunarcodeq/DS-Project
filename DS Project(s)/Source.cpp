9a223e9ef2f1c2540c41463a952b10a9d3bbd83f
Akram <akramelsadig69@gmail.com>
2025-08-16 01:04:54 -0700

    Started new project: MyDynamicArray

--- DS Project(s)/Source.cpp
+++ DS Project(s)/Source.cpp
@@ -2,56 +2,26 @@
 #include "clsDblLinkedList.h"
 #include "clsMyQueue.h"
 #include "clsMyStack.h"
+#include "clsMyDynamicArray.h"
 
 using namespace std;
 
 int main() {
 
-	clsMyStack <int> MyStack;
+	clsMyDynamicArray <int> MyArray(10);
 
-	MyStack.push(10);
-	MyStack.push(20);
-	MyStack.push(30);
-	MyStack.push(40);
-	MyStack.push(50);
-	
-	cout << "\nPrint Queue: \n";
-	MyStack.Print();
+	MyArray.SetItem(0, 10);
+	MyArray.SetItem(1, 20);
+	MyArray.SetItem(2, 30);
+	MyArray.SetItem(3, 40);
+	MyArray.SetItem(4, 50);
 
-	cout << "\n\nQueue Size: " << MyStack.Size();
-	cout << "\nQueue Front: " << MyStack.Top();
-	cout << "\nQueue Back: " << MyStack.Bottom();
+	cout << "\nIs Array Empty: " << MyArray.IsEmpty();
+	cout << "\nArray Size: " << MyArray.Size();
+	cout << "\n\nArray Items: \n";
 
-	MyStack.pop();
+	MyArray.PrintList();
 
-	cout << "\n\nQueue after poping: \n";
-	MyStack.Print();
-
-	cout << "\n\nItem(2) is: " << MyStack.GetItem(2);
-
-	MyStack.Reverse();
-	cout << "\n\nQueue after reverse : ";
-	MyStack.Print();
-
-	MyStack.UpdateItem(2, 600);
-	cout << "\n\nQueue after updating Item(2) to 600: \n";
-	MyStack.Print();
-
-	MyStack.InsertAfter(2, 800);
-	cout << "\n\nQueue after inserting 800 after 600: \n";
-	MyStack.Print();
-
-	MyStack.InsertAtFront(1000);
-	cout << "\n\nQueue after inserting 1000 at front: \n";
-	MyStack.Print();
-
-	MyStack.InsertAtBack(2300);
-	cout << "\n\nQueue after insering 2300 at the back: \n";
-	MyStack.Print();
-
-	MyStack.Clear();
-	cout << "\n\nQueue after clearing: \n";
-	MyStack.Print();
 
 	system("pause>0");
 
---
+++ DS Project(s)/clsMyDynamicArray.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <iostream>
+#include "clsDblLinkedList.h"
+
+using namespace std;
+template <class T>
+
+
+class clsMyDynamicArray
+{
+
+protected:
+
+	int _Size = 0;
+
+public:
+
+	T* _Array;
+
+	clsMyDynamicArray(int Size = 0) {
+
+		_Array = new T[Size];
+
+		_Size += Size;
+
+	}
+
+	~clsMyDynamicArray() {
+
+		delete[] _Array;
+
+	}
+
+	bool SetItem(int Index, T Value) {
+
+		if (Index >= _Size || _Size < 0)
+			return false;
+
+		_Array[Index] = Value;
+		return true;
+
+	}
+
+	void PrintList() {
+
+		for (int i = 0; i < _Size - 1; i++) {
+
+			cout << _Array[i] << " ";
+
+		}
+
+		cout << "\n";
+
+	}
+
+	bool IsEmpty() {
+
+		return (_Size == 0);
+
+	}
+
+	int Size() {
+		return _Size;
+	}
+
+};
+
